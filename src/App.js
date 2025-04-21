367c072ca34ae1be4b778bed351a16ba404d91d1
MarwanElAbhar <marwan@aklat.io>
2025-04-21 15:25:36 +0200

    fetch movies from api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const tempMovieData = [
   {
@@ -216,9 +216,27 @@ function Movie({ movie }) {
     </li>
   )
 }
+
+const KEY = "eede5d66"
 export default function App() {
-  const [movies, setMovies] = useState(tempMovieData);
-  const [watched, setWatched] = useState(tempWatchedData);
+  const [movies, setMovies] = useState([]);
+  const [watched, setWatched] = useState([]);
+  const [isLoading, setIsLoading] = useState(false)
+
+
+  useEffect(function () {
+    async function fetchMovies() {
+      setIsLoading(true)
+      const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`)
+      const data = await res.json()
+      setMovies(data.Search)
+      setIsLoading(false)
+
+    }
+    fetchMovies()
+  }, [])
+
+
 
 
   return (
@@ -229,7 +247,7 @@ export default function App() {
       </NavBar>
       <Main>
         <Box>
-          <MoviesList movies={movies} />
+          {isLoading ? <Loader /> : <MoviesList movies={movies} />}
         </Box>
         <Box>
           <WatchedSummary watched={watched} />
@@ -238,4 +256,8 @@ export default function App() {
       </Main>
     </div>
   );
+}
+
+function Loader() {
+  return <p className="loader">Loading...</p>
 }
